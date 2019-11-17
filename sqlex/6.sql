fc1a7f9c9a161ecabf76e7d26450db938a08b1cf
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2019-11-17 18:48:31 +0300

    Update 6.sql

--- sqlex/6.sql
+++ sqlex/6.sql
@@ -1,5 +1,5 @@
 /*http://www.sql-ex.ru/learn_exercises.php?LN=6*/
 
 SELECT DISTINCT maker, speed 
-FROM laptop l INNER JOIN product p on l.model = p.model 
+FROM laptop l INNER JOIN product p ON l.model = p.model 
 WHERE l.hd >= 10
