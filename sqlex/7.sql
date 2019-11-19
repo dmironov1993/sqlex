93d74c1c88c7d75a5f365ef7e0959596b8ef8199
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2019-11-19 13:45:57 +0300

    Create 7.sql

---
+++ sqlex/7.sql
@@ -0,0 +1,7 @@
+/*http://www.sql-ex.ru/learn_exercises.php?LN=7*/
+
+select product.model, pc.price from product inner join pc on product.model = pc.model where product.maker = 'B'
+union
+select product.model, laptop.price from product inner join laptop on product.model = laptop.model where product.maker = 'B'
+union 
+select product.model, printer.price from product inner join printer on product.model = printer.model where product.maker = 'B'
