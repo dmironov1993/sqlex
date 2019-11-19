f969b1c887b5866af5bbffffaa4e90572a9f6a01
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2019-11-19 13:58:36 +0300

    Update and rename 9.sql to 009.sql

--- sqlex/009.sql
+++ sqlex/009.sql
@@ -2,4 +2,8 @@
 
 /*Find the makers of PCs with a processor speed of 450 MHz or more. Result set: maker.*/
 
-SELECT DISTINCT maker FROM product WHERE model IN (SELECT model FROM pc WHERE speed >= 450)
+SELECT DISTINCT maker 
+FROM product 
+WHERE model IN (SELECT model 
+                FROM pc 
+                WHERE speed >= 450)
