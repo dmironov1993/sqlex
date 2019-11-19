27269d50a32309f8fa8a6a403fbaa7767aa4745a
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2019-11-19 13:48:01 +0300

    Update 8.sql

--- sqlex/8.sql
+++ sqlex/8.sql
@@ -1,5 +1,7 @@
 /*http://www.sql-ex.ru/learn_exercises.php?LN=8*/
 
+/*Find the makers producing PCs but not laptops.*/
+
 SELECT DISTINCT maker 
 FROM product 
 WHERE type = 'PC' AND maker NOT IN (SELECT maker FROM product p1 WHERE p1.type = 'Laptop')
