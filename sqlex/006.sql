21fd120ebe22b0149d3f01639ea8197cc482e589
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2019-11-19 13:55:33 +0300

    Update and rename 6.sql to 006.sql

--- sqlex/006.sql
+++ sqlex/006.sql
@@ -1,5 +1,8 @@
 /*http://www.sql-ex.ru/learn_exercises.php?LN=6*/
 
+/*For each maker producing laptops with a hard drive capacity of 10 Gb or higher,*/ 
+/*find the speed of such laptops. Result set: maker, speed.*/
+
 SELECT DISTINCT maker, speed 
 FROM laptop l INNER JOIN product p ON l.model = p.model 
 WHERE l.hd >= 10
