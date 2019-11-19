96e99cab7d011f03273752ed7c0ba925fdb363fd
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2019-11-19 13:48:52 +0300

    Create 9.sql

---
+++ sqlex/9.sql
@@ -0,0 +1,5 @@
+/*http://www.sql-ex.ru/learn_exercises.php?LN=9*/
+
+/*Find the makers of PCs with a processor speed of 450 MHz or more. Result set: maker.*/
+
+SELECT DISTINCT maker FROM product WHERE model IN (SELECT model FROM pc WHERE speed >= 450)
