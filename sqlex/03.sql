2c04f57baf4058e65b912f4ce56b11fcaf59edb1
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2019-11-19 13:52:55 +0300

    Update and rename 3.sql to 03.sql

--- sqlex/03.sql
+++ sqlex/03.sql
@@ -1,4 +1,5 @@
 /*http://www.sql-ex.ru/learn_exercises.php?LN=3*/
+/*Find the model number, RAM and screen size of the laptops with prices over $1000.*/
 
 SELECT DISTINCT model, RAM, screen
 FROM Laptop
