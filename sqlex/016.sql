783b197e332574b7e6f06229650f5b490255576a
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2019-12-07 23:10:59 +0300

    Update 016.sql

--- sqlex/016.sql
+++ sqlex/016.sql
@@ -1,5 +1,8 @@
 /*http://www.sql-ex.ru/learn_exercises.php?LN=16*/
 
+/*Get pairs of PC models with identical speeds and the same RAM capacity. Each resulting pair should be displayed only once, i.e. (i, j) but not (j, i). 
+Result set: model with the bigger number, model with the smaller number, speed, and RAM.*/
+
 SELECT DISTINCT pc1.model, pc2.model, pc1.speed, pc1.ram 
 FROM pc pc1, pc pc2 
 WHERE pc1.speed = pc2.speed AND pc1.ram = pc2.ram AND pc1.model > pc2.model
