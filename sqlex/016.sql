f92685c163788f32b300773a2122f715aa328914
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2019-12-07 23:10:28 +0300

    Update 016.sql

--- sqlex/016.sql
+++ sqlex/016.sql
@@ -1,3 +1,5 @@
+/*http://www.sql-ex.ru/learn_exercises.php?LN=16*/
+
 SELECT DISTINCT pc1.model, pc2.model, pc1.speed, pc1.ram 
 FROM pc pc1, pc pc2 
 WHERE pc1.speed = pc2.speed AND pc1.ram = pc2.ram AND pc1.model > pc2.model
