23564c0015633f5192c3f6a5db3bf4d6f1eaf9d1
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2020-01-14 16:56:36 -0600

    Delete 022.sql

--- sqlex/022.sql
+++
@@ -1,6 +0,0 @@
-/*Find out the maximum PC price for each maker having models in the PC table. Result set: maker, maximum price.*/
-/*http://www.sql-ex.ru/learn_exercises.php?LN=21*/
-
-SELECT product.maker, max(price) 
-FROM product INNER JOIN pc ON product.model = pc.model 
-GROUP BY product.maker
